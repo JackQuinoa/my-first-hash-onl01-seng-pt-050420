60381b838b7004547139c23167a36569498380f3
Jack Quinoa <jquinoa208@gmail.com>
2020-05-16 06:09:17 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,7 @@ end
 
 
 def shipping_manifest
-  the_manifest = ["whale_bone_corssets"=>"5", "porcelain_vases"=>"5", "oil_paintings"=>"3"]
+  the_manifest = ["whale bone corssets"=>"5", "porcelain vases"=>"5", "oil paintings"=>"3"]
 end
 
 def retrieval
@@ -13,9 +13,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+              shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -26,11 +24,11 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+shipping_manifest["muskets"]="2"
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+shipping_manifest["gun powder"]="4"
 
   # return the shipping_manifest hash below
 
